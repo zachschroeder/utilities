13a9c5dfe30f163d4e16fda8dcc420edbf75bdb1
Zach Schroeder <zach.schroeder17@gmail.com>
2022-08-16 13:01:43 -0400

    Changed path to use ~

--- ohmyposh/Microsoft.PowerShell_profile.ps1
+++ ohmyposh/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init pwsh --config C:\Users\Zach\AppData\Local\Programs\oh-my-posh\themes\zps.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\zps.omp.json | Invoke-Expression
 
 if ($env:TERM_PROGRAM -eq "vscode") {
   Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
